f19455b8a6dd3fd87235b42027ff8cacb9c49103
njoshi20 <75692444+njoshi20@users.noreply.github.com>
2022-09-17 17:56:33 -0700

    new code added

---
+++ self_assigned_projects.py
@@ -0,0 +1,8 @@
+#Using double split
+#Goal - To extract the words after '@' i.e. name of the school
+
+receivedmessage_email = "From nehajoshi@asu.com Sat Jan 5 09:14:16 2008"
+words = receivedmessage_email.split()
+email = words[1]
+part = email.split('@')
+print(part[1])
