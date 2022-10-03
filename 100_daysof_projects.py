7dfef5c42982e7e7d140a85324b4ce215136262b
njoshi20 <75692444+njoshi20@users.noreply.github.com>
2022-10-03 15:26:56 -0700

    day 2 exercises updated

--- 100_daysof_projects.py
+++ 100_daysof_projects.py
@@ -31,35 +31,27 @@
 #new_num_char = str(num_char)
 #print("Your name has " + new_num_char + " characters.")
 
-#5. Tip calculator
+#5. Total bill + tip calculator
 #result needs to be two decimal values long
 
-print("Welcome to the tip calculator")
-total_bill = float(input("What was the total bill?\n"))
-num_of_people = int(input("How many people to split the bill?\n"))
-tip_percentage_given = int(input("What percentage tip would you like to give? 10, 12 or 15?\n"))
-bill_split_per_person = (total_bill/num_of_people)
-#print(bill_split_per_person)
-percentage_split_per_person= bill_split_per_person/100*bill_split_per_person
-#print(percentage_split_per_person)
-total_bill_split_after_tip = bill_split_per_person + percentage_split_per_person
-final_amount = round(total_bill_split_after_tip, 2)
-print(f"Each person should pay ${final_amount} for the food")
+#print("Welcome to the tip calculator")
+#total_bill = float(input("What was the total bill? \n$"))
+#num_of_people = int(input("How many people to split the bill? \n"))
+#group_tip = int(input("What percentage tip would you like to give? 10, 12 or 15? \n"))
+#individual_tip_amount = (group_tip*total_bill)/100/num_of_people
+#print(individual_tip_amount)
+#total_individual_bill = round(individual_tip_amount + total_bill/num_of_people, 2)
+#total_individual_bill = "{:.2f}".format(individual_tip_amount + total_bill/num_of_people)
+#print(f"Each person should pay ${total_individual_bill}")
 
-
-
-
-
-#print("Each person should pay: " + str(x/y)/(z/100)+x/y)))
-
-#5.Write a program that adds the digits in a 2 digit number.
+#6.Write a program that adds the digits in a 2 digit number.
 # e.g. if the input was 35, then the output should be 3 + 5 = 8
 #first_digit = int(two_digit_number[0])
 #second_digit = int(two_digit_number[1])
 #two_digit_number = first_digit + second_digit
 #print(two_digit_number)
 
-#BMI Calculator
+#7. BMI Calculator
 
 #height = input("enter your height in m: ")
 #weight = input("enter your weight in kg: ")
@@ -68,19 +60,21 @@ print(f"Each person should pay ${final_amount} for the food")
 
 #print(round(3+4.41, 2))
 
-#How many days, weeks, months are you left with if you were to servive 90 years in total.
-age = input("What is your current age?")
-days = 90*365 - int(age)*365
-weeks = 90*52 - int(age)*52
-months = 90*12 - int(age)*12
-print(f"you have {days} days, {weeks} weeks, and {months} months left")
+#8. How many days, weeks, months are you left with if you were to servive 90 years in total.
+#age = input("What is your current age?")
+#days = 90*365 - int(age)*365
+#weeks = 90*52 - int(age)*52
+#months = 90*12 - int(age)*12
+#print(f"you have {days} days, {weeks} weeks, and {months} months left")
 
-#Another way, longer way of solving the above problem
-age = input("What is your current age?")
-age_as_in = int(age)
-years_remaining = 90 - age_as_in
-days_remaining = years_remaining*365
-weeks_remaining = years_remaining*52
-months_remaining = years_remaining*12
-message = (f"you have {days} days, {weeks} weeks, and {months} months left")
-print(message)
+#8.1Another way, longer way of solving the above problem
+#age = input("What is your current age?")
+#age_as_in = int(age)
+#years_remaining = 90 - age_as_in
+#days_remaining = years_remaining*365
+#weeks_remaining = years_remaining*52
+#months_remaining = years_remaining*12
+#message = (f"you have {days} days, {weeks} weeks, and {months} months left")
+#print(message)
+
+#Conditional statements exercise - treasure island game
