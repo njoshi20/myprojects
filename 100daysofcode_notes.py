ccf56709ea687630ec94e9ccffc19292fd68a462
njoshi20 <75692444+njoshi20@users.noreply.github.com>
2023-01-04 03:08:33 -0800

    100daysofcode_notes.py

--- 100daysofcode_notes.py
+++ 100daysofcode_notes.py
@@ -156,7 +156,33 @@
 # iswinning = True
 # print(f"Your score is {score}, your height is {height}, and you are winning is {iswinning}")
 # Different datatypes got converted in a str in the same line of code
-#
+
+# Life in weeks: Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
+# It will take your current age as the input and output a message with our time left in this format:
+# You have x days, y weeks, and z months left.
+# Where x, y and z are replaced with the actual calculated numbers.
+# Longer version
+# age = input("What is your current age?\n")
+# average_age = 90
+# days_in_a_year = 365
+# weeks_in_a_year = 52
+# months_in_a_year = 12
+# years_left = average_age - int(age)
+# days_left = years_left*days_in_a_year
+# weeks_left = years_left*weeks_in_a_year
+# months_left = years_left*months_in_a_year
+# print(f"You have, {days_left} days, {weeks_left} weeks, {months_left} months left.")
+
+user_age = input("What is your current age?\n")
+average_age = 90
+years_left = int(user_age)*365 - 90
+print(years_left)
+
+
+# print(f"You have, {days_left} days, {weeks_left} weeks, {months_left} months left.")
+
+
+
 
 
 
