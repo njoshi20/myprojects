0c4d80eb3d77c87c93cecb6fd42e658292fad4dd
njoshi20 <75692444+njoshi20@users.noreply.github.com>
2023-01-10 13:17:48 -0800

    new file added

--- Python_and_git_commands.py
+++ Python_and_git_commands.py
@@ -5,7 +5,15 @@
 # git remove -v
 # Note for below HEAD~0 or HEAD~1 for the needed hash
 # git reset --hard HEAD
-# 
+# -v for verbose i.e. show all
+#pwd
+# ls
+# -n dry run
 
 
 # Python
+# Functions
+    # print
+    #input
+    #len
+    #round
